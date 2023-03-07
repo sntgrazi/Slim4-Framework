a167369bf491582672f799991f62525325c5a0a6
Grazi Santana <99621932+sntgrazi@users.noreply.github.com>
2023-03-06 21:11:01 -0300

    adicionando novos arq

---
+++ slim4/env.php
@@ -0,0 +1,7 @@
+<?php 
+
+    putenv('GERNECIADOR_MYSQL_HOST=localhost');
+    putenv('GERENCIADOR_MYSQL_DBNAME=teste');
+    putenv('GERENCIADOR_MYSQL_USER=roote');
+    putenv('GERENCIADOR_MYSQL_PASSWORD=null');
+    putenv('GERENCIADOR_MYSQL_PORT=3307');
