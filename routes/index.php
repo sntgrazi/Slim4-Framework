043e1d15da0daa10185fd43918785a236cb11cd0
Grazi Santana <99621932+sntgrazi@users.noreply.github.com>
2023-03-09 16:38:51 -0300

    Add files via upload

---
+++ routes/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+use Slim\Factory\AppFactory;
+use App\Controllers\ProdutoController;
+use App\Controllers\LojaController;
+use Slim\Middleware\BodyParsingMiddleware;
+
+require "../vendor/autoload.php";
+
+
+$app = AppFactory::create();
+
+$app->addBodyParsingMiddleware();
+
+$app->AddErrorMiddleware(true,true,true);
+
+
+$app->get('/produto', ProdutoController::class . ':getProduto');
+$app->post('/produto', ProdutoController::class . ':insertProduto');
+$app->put('/produto', ProdutoController::class . ':updateProduto');
+$app->delete('/produto', ProdutoController::class . ':deleteProduto');
+
+$app->get('/loja', LojaController::class . ':getLojas');
+$app->post('/loja', LojaController::class . ':insertLoja');
+$app->put('/loja', LojaController::class . ':updateLoja');
+$app->delete('/loja', LojaController::class . ':deleteLoja');
+
+$app->run();
