5655549208789c9eaeb8287a030040a0cd2d843c
Grazi Santana <99621932+sntgrazi@users.noreply.github.com>
2023-03-09 20:50:46 -0300

    Add files via upload

--- slim4/env.php
+++ slim4/env.php
@@ -1,7 +1,7 @@
 <?php 
 
-    putenv('GERNECIADOR_MYSQL_HOST=localhost');
-    putenv('GERENCIADOR_MYSQL_DBNAME=teste');
-    putenv('GERENCIADOR_MYSQL_USER=roote');
-    putenv('GERENCIADOR_MYSQL_PASSWORD=null');
-    putenv('GERENCIADOR_MYSQL_PORT=3307');
+    putenv('DB_HOST=localhost');
+    putenv('DB_NAME=teste');
+    putenv('DB_USER=roote');
+    putenv('DB_PASSWORD=null');
+    putenv('DB_PORT=3307');
